04711b55df1cf3b49bcb59ee3175051d523b7e46
toure <abdelraul17@yahoo.fr>
2021-07-27 20:05:58 +0000

    add of model Contacts of noyau application

--- noyau/models.py
+++ noyau/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+class Contacts(models.Model):
+    tel_portable = models.CharField(max_length=50)
+    tel_domicile = models.CharField(max_length=50)
+    tel_bureau = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+
 # Create your models here.
