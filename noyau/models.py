1cda210dc524dfa82eb9642e73f039a17709850d
iliasse <iliasseg97@gmail.com>
2021-08-10 20:01:36 +0000

    Commit Iliasse Guindo

--- noyau/models.py
+++ noyau/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    tel_portable = models.CharField(max_length=30)
+    tel_bureau = models.CharField(max_length=30)
+    tel_domicile = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
